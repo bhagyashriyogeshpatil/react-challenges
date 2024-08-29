c271af874ed6939d0850157efc9761a575cc8d2d
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-08-29 18:16:03 +0000

    Add Loader component with conditional rendering in Content.js: Created Loader component and imported it in Content.js; Added conditional rendering of Loader and PostItem based on isLoaded state;  Implemented a timer to switch isLoaded to true after 2 seconds.

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isLoaded: false
+        }
     }
-    
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+
     render() {
         return (
             <div className={css.Content}>
@@ -34,7 +46,11 @@ export class Content extends Component {
 
                     {/* Part 2: Creating a child component */}
 
-                    <PostItem savedPosts={savedPosts} />
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
                 </div>
             </div>
         )

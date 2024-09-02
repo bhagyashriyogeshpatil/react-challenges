1cb64f1060ff61791be31324442c65c649bd664b
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-02 12:17:29 +0000

    Convert Content.js class component to functional component with hooks: Replaced state with useState for isLoaded and fetchedPosts; Converted componentDidMount to useEffect with a timer for loading posts; Updated JSX to remove references to 'this' and replaced with hook variables; Added handleChange to filter fetchedPosts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
 import Content from "./components/Content";
+import ContentHooks from "./components/ContentHooks";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      {/* <Content /> */}
+      <ContentHooks />
     </div>
   );
 }
